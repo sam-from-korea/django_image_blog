473b8bd39a5f874c2e9d57433ab4613db6f172e9
sam-from-korea <xsamkim70x@naver.com>
2024-10-15 15:20:57 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), # 장고가post_detail 뷰로 보내, 게시글을 볼 수 있게 urls.py수정
 ]
