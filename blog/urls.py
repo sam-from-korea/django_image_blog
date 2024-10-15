1b209f163b463c179eca73046319ef64423c0ac5
sam-from-korea <xsamkim70x@naver.com>
2024-10-15 16:13:26 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'), # 장고가post_detail 뷰로 보내, 게시글을 볼 수 있게 urls.py수정
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), # 장고가 post_detail 뷰로 보내, 게시글을 볼 수 있게 urls.py수정
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
