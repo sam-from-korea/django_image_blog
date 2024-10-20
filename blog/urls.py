c2a1c4640be5ea8e8f5c04942155367f4e331f82
sam-from-korea <xsamkim70x@naver.com>
2024-10-20 22:04:15 +0900

    image

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from django.urls import include
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register('Post', views.blogImage)
+ 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('api_root/', include(router.urls)),
     path('post/<int:pk>/', views.post_detail, name='post_detail'), # 장고가 post_detail 뷰로 보내, 게시글을 볼 수 있게 urls.py수정
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
+
+REST_FRAMEWORK= {
+    'DEFAULT_PERMISSION_CLASSES': [
+       # 'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
